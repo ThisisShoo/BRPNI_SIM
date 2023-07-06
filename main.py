8d98ab9a7632e08286e7a1abe856d90f0ee5e039
ThisisShoo <shuhan_zheng@qq.com>
2023-07-06 22:17:43 +0800

    Progress save
    Field image of a bar magnet can be successfully generated. However, the problem where POV can not be switched remains unsolved.

--- main.py
+++ main.py
@@ -6,14 +6,16 @@ from matplotlib import pyplot as plt
 import config
 import config_fns
 import ray_tracing_fns
+import os
 
 # Configuration
 DATA_PATH = "E:/Documents and stuff/School_Stuff/_CSNS/PNI/COMSOL6.0(64bit)/Simulations/"
+# DATA_FILE = "Horseshoe.txt"
 DATA_FILE = "bar_magnet.txt"
 
-AXIS = "x"
+AXIS = "z"
 
-PLOT_FIELD_FIRST = False # If true, generates a plot of the field before raytracing
+PLOT_FIELD_FIRST = True # If true, generates a plot of the field before raytracing
 SOURCE_PROFILE = "gaussian" # Defines a source profile (use an imported function here)
 
 # Execution code
@@ -27,21 +29,33 @@ if __name__ == "__main__":
         field = data["field"]
         field_loc = data["field_loc"]
 
+    # Trim the data
+    field = field[1:-1, 1:-1, 1:-1, :]
+    field_loc = field_loc[:, 1:-1, 1:-1, 1:-1]
+
+    # Rotate the field
+    axis_rot = {"x": [0, 1, 2], "y": [1, 0, 2], "z": [2, 0, 1]}
+
+    field_T = np.transpose(field, (3, 0, 1, 2))
+    field_T = field_T[axis_rot[AXIS]]
+    field = np.transpose(field_T, (1, 2, 3, 0))
+
+    field_loc = field_loc[axis_rot[AXIS]]
+
     space_dim = np.shape(field)
-    space_lim = (np.array(space_dim[:-1])-2)
+    space_dim = np.array(space_dim[:-1])-2
 
     # Plot the input data
     if PLOT_FIELD_FIRST:
         ax = plt.figure().add_subplot(projection='3d')
-        field_temp = field[:-2, :-2, :-2, :]
-        field_temp = np.transpose(field_temp, (3, 0, 1, 2))
-        x, y, z = field_loc[:, :-2, :-2, :-2]
+        field_temp = np.copy(field_T)
+        x, y, z = field_loc
         ax.quiver(x, y, z,
                   field_temp[0], field_temp[1], field_temp[2],
                   length = 1, normalize = True)
-        plt.xlim(0, space_lim[0])
-        plt.ylim(0, space_lim[1])
-        ax.set_zlim(0, space_lim[2])
+        plt.xlim(0, space_dim[0])
+        plt.ylim(0, space_dim[1])
+        ax.set_zlim(0, space_dim[2])
         plt.show()
 
     if SOURCE_PROFILE is None:
@@ -57,18 +71,20 @@ if __name__ == "__main__":
     start_time = datetime.now()
     print("Starting raytracing at " + str(start_time))
 
-    output = np.zeros(space_dim[:-2])
+    output = np.zeros(space_dim[1:])
+    print(np.shape(output))
 
     PIX_COUNT = 0
     PIX_TIME = 0
     tot_pix = np.prod(space_dim[:-1])
-    with mp.Pool(processes = mp.cpu_count()) as pool:
+    with mp.Pool(processes = os.cpu_count()) as pool:
         for i, row in enumerate(output):
             for j, col in enumerate(row):
                 pix_start = datetime.now()
                 progress = round(PIX_COUNT/tot_pix * 100, 2)
                 print(f"Now processing pixel ({i}, {j}), progress {progress}%. "
-                      f"Last pixel used {PIX_TIME}.")
+                      f"Last pixel used {PIX_TIME}. "
+                      f"Total time elapsed: {datetime.now() - start_time}")
                 i_pix = pool.apply_async(ray_tracing_fns.ray_tracing_sim,
                                          args=(field, (i, j), source))
                 i_pix = i_pix.get()
